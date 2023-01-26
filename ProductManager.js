ca5dfe7615186aea76840283d12f625f5068e928
sebastiap <sebastiap@gmail.com>
2023-01-26 20:46:31 -0300

    Endpoint con express

---
+++ ProductManager.js
@@ -0,0 +1,224 @@
+import fs  from 'fs';
+const archivo = './ejemplo.txt';
+
+//Esto es solo para borrarlo rapido
+// fs.writeFileSync(archivo,"[]");
+
+export default class ProductManager{
+    constructor(){
+        this.products = [];
+        this.idIndex = 0 ;
+        this.path ="./"
+    }
+    
+    addProduct = async(product) => {
+        // Valida que no se repita el campo "code"
+        try {
+            let products = await this.getProducts();
+            if (products.length === 0) {
+                product.id = 0;
+            }
+            else {
+                product.id = products[products.length-1].id + 1;
+            }
+
+            const existingProduct = products.find((prod)=> prod.code === product.code);
+            if (existingProduct){
+                console.error('El código del producto ya existe');
+                return;
+            }
+    
+            if (!product.title || !product.description || !product.price || !product.thumbnail || !product.code || !product.stock){
+                console.log("Producto invalido. Falta ingresar algun campo.");
+                return;
+            }
+            // Agrega el producto al arreglo de productos
+            console.log(products);
+            products.push(product);
+
+            console.log("Se agrego correctamente el producto con id", product.id);
+            await fs.promises.writeFile(this.path + archivo,JSON.stringify(products));
+            return product;
+
+
+        } catch (error) {
+            console.log("error:" , error);
+        }
+
+    }
+
+    getProducts = async() => {
+        try{        
+            if (fs.existsSync(archivo)) {
+                const data = await fs.promises.readFile(this.path + archivo,'utf-8');
+                const prods = await JSON.parse(data);
+                return prods;
+            }
+            else {
+                console.warn("El archivo no existia al momento de ejecucion.");
+                return [];
+            }
+        }
+            catch(error){ 
+                console.log("error:" , error); 
+        }
+
+    }
+
+    getProductById =  async(id) => {
+        const allProducts =  await this.getProducts();
+        
+        const SearchedProduct = allProducts.find((prod)=> prod.id === id);
+
+        // Muestra un error en la consola en caso de no encontrar ningún coincidencia
+        if (!SearchedProduct) {
+            console.error('Not found');
+            return;
+          }
+        
+        return SearchedProduct;
+    }
+
+    updateProduct = async(id,product) => {
+        //Recibe todo el producto
+        const allProducts = await this.getProducts();
+        console.log("Productos al momento de updetear :", allProducts)
+        const SearchedProductindex = allProducts.findIndex((prod)=> prod.id === id);
+        console.log("Encontre para updetear :", SearchedProductindex)
+        if (SearchedProductindex < 0 ) {
+            console.error('No existe con ese id');
+            return;
+        }
+        product.id = id;
+        allProducts[SearchedProductindex] = product;
+         // Para evitar problemas cuando el tamaño del archivo se reduce, lo trunco y lo recreo.
+        // fs.truncateSync(this.path + archivo);
+        await fs.writeFileSync(this.path + archivo,JSON.stringify(allProducts));
+        console.log("Se actualizo satisfactoriamente el producto con id", id);
+
+        return allProducts;
+
+    }
+    deleteProduct = async(id) => {
+        const allProducts = await this.getProducts();
+
+        // console.log("Productos al momento de eliminar :", allProducts)
+        const SearchedProductindex = allProducts.findIndex((prod)=> prod.id === id);
+        // console.log("Encontre para eliminar :", SearchedProductindex)
+        if (SearchedProductindex < 0 ) {
+            console.error('No existe un producto con ese id');
+            return;
+        }
+        allProducts.splice(SearchedProductindex, 1);
+        console.log("Se elimino satisfactoriamente el producto con id", id);
+        // console.log("Los productos que quedaron son :", allProducts);
+
+        // Para evitar problemas cuando el tamaño del archivo se reduce, lo trunco y lo recreo.
+        // fs.truncateSync(this.path + archivo);
+        await fs.writeFileSync(this.path + archivo,JSON.stringify(allProducts));
+        return allProducts;
+    }
+}
+
+const test = async () => {
+
+
+// Se creará una instancia de la clase “ProductManager”
+let productos = new ProductManager;
+//Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío []
+let compra = await productos.getProducts();
+
+
+console.log("\n* Se creará una instancia de la clase 'ProductManager' y Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío []");
+console.log("Productos:", compra,"\n");
+
+// Se llamará al método “addProduct” con los campos:
+let ProductoPrueba = {
+    title : 'producto prueba',
+    description : 'Este es un producto prueba',
+    price : 200,
+    thumbnail : 'Sin imagen',
+    code : 'abc123',
+    stock : 25,
+}
+// El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE
+console.log("* Se llamará al método “addProduct”, el objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE");
+await productos.addProduct(ProductoPrueba);
+
+//Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado
+compra =  await productos.getProducts();
+console.log("\n* Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado \n");
+console.log("Productos:", compra);
+
+//Se evaluará que getProductById devuelva error si no encuentra el producto o el producto en caso de encontrarlo
+console.log("\n* Si busco un producto no existente... ");
+await productos.getProductById(10);
+
+console.log("\n* Si busco el producto que ingrese...");
+let seleccionado = await productos.getProductById(0);
+console.log("Producto seleccionado:", seleccionado)
+
+console.log("\n* Ingreso 3 productos de relleno...");
+let Producto1 = {
+    title : 'producto 1',
+    description : 'Este es el primer producto extra.',
+    price : 1200,
+    thumbnail : 'Sin imagen',
+    code : 'abc1',
+    stock : 25,
+}
+let Producto2 = {
+    title : 'producto 2',
+    description : 'Este es el segundo producto extra.',
+    price : 2000,
+    thumbnail : 'Sin imagen',
+    code : 'abc2',
+    stock : 25,
+}
+let Producto3 = {
+    title : 'producto 3',
+    description : 'Este es el tercer producto extra.',
+    price : 3200,
+    thumbnail : 'Sin imagen',
+    code : 'abc3',
+    stock : 25,
+}
+
+
+await productos.addProduct(Producto1);
+await productos.addProduct(Producto2);
+await productos.addProduct(Producto3);
+
+compra = await productos.getProducts();
+console.log("\n* Se llamará el método “getProducts” nuevamente, esta vez deben aparecer 3 productos mas. \n");
+console.log("Productos:", compra);
+
+let ProductoUpdeteado = {
+    title : 'producto Updeteado',
+    description : 'Este es un producto prueba pero nuevo',
+    price : 3200,
+    thumbnail : 'Sin imagen',
+    code : 'abc3',
+    stock : 25,
+}
+
+console.log("\n* Se llamará al método “updateProduct” y se intentará cambiar un campo de algún producto, se evaluará que no se elimine el id y que sí se haya hecho la actualización. \n");
+console.log("\n* Se actualizara el producto de id 3 \n");
+await productos.updateProduct(3,ProductoUpdeteado);
+console.log("Productos:", compra);
+
+console.log("\n* Se llamará al método “deleteProduct”, se evaluará que realmente se elimine el producto \n");
+console.log("\n* Se eliminara el producto de id 2 \n");
+// await productos.deleteProduct(2);
+
+console.log("\n* Se llamará nuevamente al método “deleteProduct”, se evaluará que arroje un error en caso de no existir el producto \n");
+console.log("\n* Se intentara eliminar el producto de id 15 \n");
+// await productos.deleteProduct(15);
+
+console.log("\n* Se llamará el método “getProducts” para verificar el estado final. \n");
+compra =  await productos.getProducts();
+console.log("Productos:", compra);
+}
+
+// Solo para probar y cargar el archivo
+// test();
---
+++ app.js
@@ -0,0 +1,37 @@
+import express from "express";
+import ProductManager from "./ProductManager.js";
+
+const app = express();
+const manager = new ProductManager;
+app.use(express.urlencoded({extended:true}));
+app.get('/products/:idproducto',async(req,res) =>{
+const producto = parseInt(req.params.idproducto);
+
+let SearchedProduct = await manager.getProductById(producto);
+if (!SearchedProduct){
+    let text = "No se encontro ningun producto con el id " + producto;
+    res.send({error:{text}});
+
+}
+else {
+   res.send(SearchedProduct);
+}
+
+
+});
+
+app.get("/products",async(req,res) =>{
+
+    let limit = req.query.limit;
+    let fileproductos = await manager.getProducts();
+    if (limit){
+        let n = fileproductos.length;
+        if (n > limit) {
+            n = limit;
+        }
+        fileproductos = fileproductos.slice(0, n);
+    }
+    res.send(fileproductos);
+})
+
+app.listen(8080, ()=> console.log('listening on port 8080'));
