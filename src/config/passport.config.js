85775a0568743fe92fe8dd2f34d2a07be38a1cea
sebastiap <sebastiap@gmail.com>
2023-07-02 18:39:35 -0300

    github test redirect_uri

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -85,8 +85,7 @@ const initializePassport = () => {
         clientID:config.githubId, 
         clientSecret:config.githubSecret,
         // callbackURL:'http://localhost:'+ config.port + '/auth/github-callback'
-        callbackURL: config.localhost +':'+ config.port + '/auth/github-callback',
-        redirect_uri:'http://localhost:'+ config.port + '/auth/github-callback'
+        callbackURL: config.localhost +':'+ config.port + '/auth/github-callback'
     }, async (accessToken,refreshToken,profile,done) => {
         try {
             const user = await manager.getOne(profile._json.email);
